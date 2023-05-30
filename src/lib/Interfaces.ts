dc4988f3e6b16a7faaac1879efa8753d9edf3794
GatereKinyanjui <gaterekinyanjui@gmail.com>
2023-05-30 18:22:09 +0300

    fires first deployment test

---
+++ src/lib/Interfaces.ts
@@ -0,0 +1,9 @@
+interface IClientUserCredentials {
+  id: string;
+  firstName: string;
+  secondName: string;
+  email: string;
+  phoneNumber: string;
+  password: string;
+  confirmPassword: string;
+}
